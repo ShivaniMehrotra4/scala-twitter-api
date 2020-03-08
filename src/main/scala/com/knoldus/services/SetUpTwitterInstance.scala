fdfc351c18d057fd6503948f6dbb9d66889ef0b8
ShivaniMehrotra4 <shivanimehrotra.sms@gmail.com>
2020-03-08 20:15:08 +0530

    updated source code

--- src/main/scala/com/knoldus/services/SetUpTwitterInstance.scala
+++ src/main/scala/com/knoldus/services/SetUpTwitterInstance.scala
@@ -1,8 +1,45 @@
-package com.knoldus.controller
+package com.knoldus.services
+
+import com.typesafe.config.ConfigFactory
+import twitter4j.auth.AccessToken
+import twitter4j.{Query, QueryResult, Status, Twitter, TwitterFactory}
+import scala.collection.JavaConverters._
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
+import scala.language.implicitConversions
+
+class SetUpTwitterInstance {
+
+  def getInstanceTwitter: Twitter = {
+
+    val config = ConfigFactory.load()
+    val twitter: Twitter = new TwitterFactory().getInstance()
+    twitter.setOAuthConsumer(config.getString("consumer.key"), config.getString("consumer.secret"))
+    twitter.setOAuthAccessToken(new AccessToken(config.getString("token.key"), config.getString("token.secret")))
+    twitter
+
+  }
+
+  def getTweets(twitter: Twitter,hashTag :String): Future[List[Status]] = {
+    val query = new Query(hashTag)
+    val queryResult: Future[QueryResult] = Future(twitter.search(query))
+    val futureListOfTweets: Future[List[Status]] = queryResult.map(status => status.getTweets.asScala.toList)
+    futureListOfTweets
+  }.fallbackTo(Future {
+    List.empty[Status]
+  })
+}
+
+
+
+
+/*
+package com.knoldus.services
 
 import com.typesafe.config.ConfigFactory
 import twitter4j.auth.AccessToken
 import twitter4j.{Twitter, TwitterFactory}
+
 import scala.language.implicitConversions
 
 class SetUpTwitterInstance {
@@ -19,5 +56,4 @@ class SetUpTwitterInstance {
 }
 
 
-
-
+ */
--- src/main/scala/com/knoldus/services/TwitterOperations.scala
+++ src/main/scala/com/knoldus/services/TwitterOperations.scala
@@ -1,6 +1,57 @@
-package com.knoldus.controller
+package com.knoldus.services
+
+import twitter4j.Status
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
+import scala.language.implicitConversions
+
+class TwitterOperations {
+
+
+  def getNumberOfTweets(futureListOfTweets: Future[List[Status]]): Future[Int] = {
+    futureListOfTweets.map(listOfTweets => listOfTweets.length)
+  }.fallbackTo(Future {
+    -1
+  })
+
+  def getTweetLikes(futureListOfTweets: Future[List[Status]]): Future[List[Int]] = {
+    val futureFavCount = futureListOfTweets.map(listOfTweets => listOfTweets.map(tweets => tweets.getFavoriteCount))
+    futureFavCount
+  }.fallbackTo(Future {
+    List.empty[Int]
+  })
+
+  def getTweetsPerDay(futureListOfTweets: Future[List[Status]]): Future[Int] = {
+    val res = futureListOfTweets.map(listOfTweets => listOfTweets.map(tweets => tweets.getCreatedAt.getTime).distinct.length)
+    val res1: Future[Int] = futureListOfTweets.map(v => res.map(v.length / _)).flatten
+    res1
+  }.fallbackTo(Future {
+    -1
+  })
+
+  def getAverageOfFavCount(futureFavCount: Future[List[Int]]): Future[Int] = {
+    val futureSum = futureFavCount.map(_.sum)
+    val avg: Future[Int] = futureSum.map(sum => futureFavCount.map(sum / _.length)).flatten
+    avg
+  }.fallbackTo(Future {
+    -1
+  })
+
+  def getRetweetCount(futureListOfTweets: Future[List[Status]]): Future[List[Int]] = {
+    val retweet = futureListOfTweets.map(listOfTweets => listOfTweets.map(tweets => tweets.getRetweetCount))
+    retweet
+  }.fallbackTo(Future {
+    List.empty[Int]
+  })
+}
+
+
+/*
+package com.knoldus.services
 
 import twitter4j.{Query, QueryResult, Status, Twitter}
+
 import scala.collection.JavaConverters._
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
@@ -57,3 +108,5 @@ class TwitterOperations {
     List.empty[Int]
   })
 }
+
+ */
